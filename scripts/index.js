ee3eab10e2d676b1d42af42efeba9bf56561d5c0
vadimlasevich <lasevichv@mail.ru>
2023-01-18 20:39:10 +0300

    feat: add js code(input, tabs, select)

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+const headerLinkDisplay = document.querySelector('.header__subtitle-link-display');
+const headerSubtitle = document.querySelector('.header__subtitle');
+const headerSubtitleLinkDisplay = document.querySelector('.header__subtitle-link-display span');
+const headerSubtitleLinkArrow = document.querySelector('.header__subtitle-link-display .table-arrow-btn');
+const headerInput = document.querySelector('.header__input input');
+const headerInputBtnClose = document.querySelector('.header__input-btn-close');
+
+const investmentsStockTableBtns = document.querySelectorAll('.investments-stock__table-btn');
+const investmentsStockTableItems = document.querySelectorAll('.investments-stock__table-items');
+const select = document.querySelector('select');
+
+const toggleShowText = () => {
+  headerSubtitle.classList.toggle('hidden-text');
+
+  if (!headerSubtitle.classList.contains('hidden-text')) {
+    headerSubtitleLinkDisplay.textContent = 'Скрыть';
+    headerSubtitleLinkArrow.classList.add('arrow-btn-rotate');
+  } else {
+    headerSubtitleLinkDisplay.textContent = 'Показать весь';
+    headerSubtitleLinkArrow.classList.remove('arrow-btn-rotate');
+  }
+};
+
+const displayCloseBtn = ({ target }) => {
+  if (target.value) {
+    headerInputBtnClose.style.display = 'block';
+  } else {
+    headerInputBtnClose.style.display = 'none';
+  }
+};
+
+investmentsStockTableBtns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    investmentsStockTableBtns.forEach((btn) => btn.classList.remove('table-btn-active'));
+    investmentsStockTableItems.forEach((item) => item.classList.add('hidden'));
+    investmentsStockTableBtns[index].classList.add('table-btn-active');
+    investmentsStockTableItems[index].classList.remove('hidden');
+  });
+});
+
+headerLinkDisplay.addEventListener('click', toggleShowText);
+headerInput.addEventListener('input', displayCloseBtn);
+headerInputBtnClose.addEventListener('click', () => {
+  headerInput.value = '';
+  headerInputBtnClose.style.display = 'none';
+});
+
+// function sel() {
+//   select.selectric();
+// }
+
+// sel();
+$(function () {
+  $('select').selectric();
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   $('select').selectric();
+// });
+
+// console.log($('select'));
